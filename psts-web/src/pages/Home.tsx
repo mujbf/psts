d65d480a08dc12b2bf74e2989dd56040e8b4e622
nisaldevinda <nisaldevindar@gmail.com>
2025-02-11 10:01:42 +0530

    knob 2

--- psts-web/src/pages/Home.tsx
+++ psts-web/src/pages/Home.tsx
@@ -3,12 +3,13 @@ import Col1 from "../components/sections/Col1";
 import Side2 from "../components/sections/Side2";
 import Col2 from "../components/sections/Col2";
 import Col3 from "../components/sections/Col3";
+import KnobCarousel from "../components/sections/KnobCarousel";
 
 const Home = () => {
   return (
     <>
-      <div className="w-screen h-screen flex items-center justify-center">
-        <p className="montserrat-title">Home Knob Section</p>
+      <div className="w-screen h-screen relative overflow-hidden primary-gradient z-1">
+        <KnobCarousel rotationInterval={10000} />
       </div>
       <Col1 />
       <Side2 />
